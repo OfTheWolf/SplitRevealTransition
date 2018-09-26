430558ccf6bee303eb18bd141b0407269ebcb3b3
ugur <uguboz@gmail.com>
2018-09-26 21:30:53 +0300

    Initial Commit

---
+++ SplitRevealTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SplitRevealTransition
+//
+//  Created by ugur on 26.09.2018.
+//  Copyright © 2018 OfTheWolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
